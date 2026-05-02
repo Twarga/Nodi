7f415c62643db406c8871eb1528698e15f7101e7
Twarga <twarga.touzani.05@gmail.com>
2026-05-02 17:22:06 +0100

    T135-T136: Remove front-end inspiration from git and delete test.html

--- front-end inspiration/src/lib/useFsSnapshot.ts
+++
@@ -1,9 +0,0 @@
-import { useSyncExternalStore } from "react";
-import { store } from "./fileStore";
-
-export const useFsSnapshot = () =>
-  useSyncExternalStore(
-    (cb) => store.subscribe(cb),
-    () => store.entries,
-    () => store.entries,
-  );
