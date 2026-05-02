562ec7a3ef322518d2e9a7590aa6430545452a26
Twarga <youness.touzani.03@gmail.com>
2026-05-02 05:58:53 +0100

    T9

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,9 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"nodi/internal/config"
 )
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+		next.ServeHTTP(rw, r)
+		
+		duration := time.Since(start)
+		log.Printf("%s %s %d %v", r.Method, r.URL.Path, rw.statusCode, duration)
+	})
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
 
 func main() {
 	cfg, err := config.Load()
@@ -22,6 +45,8 @@ func main() {
 		fmt.Fprintf(w, "Nodi is running! Root: %s", cfg.Root)
 	})
 
+	handler := loggingMiddleware(mux)
+
 	log.Printf("Listening on :%s", cfg.Port)
-	log.Fatal(http.ListenAndServe(":"+cfg.Port, mux))
+	log.Fatal(http.ListenAndServe(":"+cfg.Port, handler))
 }
